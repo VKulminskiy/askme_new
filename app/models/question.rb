bf3d52c1543cb35bf45eceb85f48984b145d30ae
wkylminskii <w_kylminskii@mail.ru>
2020-06-10 00:33:31 +0300

    New validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ApplicationRecord
+  belongs_to :user
+  validates :user, :text, presence: true
+
+  # Проверка максимальной длины текста вопроса (максимум 255 символов)
+  validates :text, length: { maximum: 255 }
 end
