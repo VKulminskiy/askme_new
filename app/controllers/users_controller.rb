dc4fc3b7b76bd5388faf445bdf3065dc7bdd74d9
wkylminskii <w_kylminskii@mail.ru>
2020-06-17 15:03:04 +0300

    Add styles, jquery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,34 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+      id: 1,
+      name: 'Viacheslav',
+      username: 'slavjan',
+      avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5w0jXPEZumrhYIsJS_kL8RNPN8hxp7OfwEx000lrx2weiKySU&usqp=CAU'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-  	@time = Time.now
-	@hello = "Как здорово, что все мы здеь сегодня собрались!"
+    @user = User.new(
+      name: 'Viacheslav',
+      username: 'slavjan',
+      avatar_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5w0jXPEZumrhYIsJS_kL8RNPN8hxp7OfwEx000lrx2weiKySU&usqp=CAU'
+      )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016'))
+    ]
+
+    @new_question = Question.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
+
+  def words_form(number, enot, enota, enotov)
+    ostatok = number % 10
+
+    return enotov if number.between?(11, 14) || number % 100 == 11
+
+    return enot if ostatok == 1
+
+    return enota if ostatok.between?(2, 4)
+
+    return enotov if ostatok.between?(5, 9) || ostatok == 0
+  end
 end
