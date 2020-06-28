1e8b2c29ef26830225f1d2eca946ac4ebac95ad2
wkylminskii <w_kylminskii@mail.ru>
2020-06-28 15:38:52 +0300

    authorization after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      # в объект session записываем идентификатор пользователя
+      session[:user_id] = @user.id
+
       redirect_to root_url, notice: 'Пользователь успешно зарегестрирован!'
     else
       render 'new'
