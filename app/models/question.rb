13bb70a1fb98815a1bede6ee9e391f0155b1b04d
wkylminskii <w_kylminskii@mail.ru>
2020-07-17 00:26:20 +0300

    add author to question, optional

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  # Если установить :optional в true, тогда наличие связанных объектов не будет валидироваться. По умолчанию установлено в false
+  belongs_to :author, class_name: 'User', optional: true
 
   validates :text, presence: true, length: { maximum: 255 }
 end
